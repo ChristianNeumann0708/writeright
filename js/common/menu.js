97d2d4f2937380fb6ce875a7da589ff4e13d1450
ChristianNeumann0708 <stasyszyn@t-online.de>
2026-03-04 14:18:03 +0100

    Feature: Finalized Einmaleins HTML UI layout matching Vokabeltrainer layout

--- js/common/menu.js
+++ js/common/menu.js
@@ -24,6 +24,15 @@ export function loadMenu() {
           Vokabeltrainer
         </a></li>
 
+        <li><a href="einmaleins.html">
+          <svg width="20" height="20" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round">
+            <rect x="3" y="3" width="18" height="18" rx="2" ry="2"></rect>
+            <line x1="9" y1="9" x2="15" y2="15"></line>
+            <line x1="15" y1="9" x2="9" y2="15"></line>
+          </svg>
+          Einmaleins
+        </a></li>
+
         <li><a href="import.html">
           <svg width="20" height="20" viewBox="0 0 24 24" fill="none">
             <path d="M12 3v12m0 0l-4-4m4 4l4-4m-9 5v4h10v-4" 
